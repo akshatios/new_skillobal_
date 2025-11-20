f404407bd61d9054ddb4f0fb959b066256f84ab8
SOFT-14 <tarunsingh2002office@gmail.com>
2025-11-20 20:06:40 +0530

    ai feature documenattaion is also added

--- ai_features/aiFeatureRoutes.py
+++ ai_features/aiFeatureRoutes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+from documentation.aiFetureDocumentation import LactureQuestionAnswerGenerationModel
 from ai_features.views.QuestionAnswerGenerationModel import QuestionAnswerGenerationModel
 
 aiFeatureRoutes = APIRouter(prefix="/Ai_Features", tags=["AI"])
 
 
-aiFeatureRoutes.add_api_route("/LactureQuestionAnswerGenerationModel", QuestionAnswerGenerationModel, methods=["POST"])
+aiFeatureRoutes.add_api_route("/LactureQuestionAnswerGenerationModel", QuestionAnswerGenerationModel, methods=["POST"], description=LactureQuestionAnswerGenerationModel)
