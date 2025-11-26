761abb35e5f5380883915086564341949ed37219
akshatios <akshatjain9989@gmail.com>
2025-11-26 16:54:54 +0530

    Fix video processing: handle videos without audio tracks

--- core/routes.py
+++ core/routes.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from user.userRoutes import user_router
 from courses.coursesRoutes import courses_router
+
+# from dashboard.dashboard_router import dashboard_router
+from ai_features.aiFeatureRoutes import aiFeatureRoutes
+
 from dashboard.dashboardRoutes import dashboard_router
 from languages.languagesRoutes import router as languages_router
 from categories.categoriesRoutes import router as categories_router
@@ -11,6 +15,9 @@ api_router = APIRouter()
 # Include all module routers
 api_router.include_router(user_router)
 api_router.include_router(courses_router)
+# api_router.include_router(slider_router)
+api_router.include_router(aiFeatureRoutes)
+
 api_router.include_router(dashboard_router)
 api_router.include_router(languages_router, prefix="/admin/languages", tags=["Languages"])
 api_router.include_router(categories_router, prefix="/admin/categories", tags=["Categories"])
